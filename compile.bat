a406575c843d330aee3224bd2c5d0e679a595ef8
ithirzty <45016834+ithirzty@users.noreply.github.com>
2021-06-11 15:54:58 +0200

    updated compile script

--- compile.bat
+++ compile.bat
@@ -1,1 +1,15 @@
 go build -o "C:/Windows/goact.exe" .
+
+set GOARCH=amd64
+set GOOS=linux
+go build -o "linux_amd64" .
+
+@REM set GOARCH=amd64
+@REM set GOOS=darwin
+@REM go tool dist install -v pkg/runtime
+@REM go install -v -a std
+@REM go build -ldflags -H=windowsgui -o "jvc_macos.app" .
+
+set GOARCH=amd64
+set GOOS=windows
+go build -o "windows_amd64.exe" .
